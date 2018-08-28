7201544f561def788701819d8099d0e9a1ac0d12
James Yoder <james@jyoder.com>
2018-08-28 07:39:20 -0500

    wip

--- scripts/update_src.bash
+++ scripts/update_src.bash
@@ -4,12 +4,17 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo Updating vr_top...
 git pull
+echo Updating gss...
 cd $DIR/../src/gss
 git pull
+echo Updating rscore...
 cd $DIR/../src/rscore
 git pull
+echo Updating gbx2ros...
 cd $DIR/../src/gbx2ros
 git pull
+echo Updating gnss_visualization...
 cd $DIR/../src/gnss_visualization
 git pull
