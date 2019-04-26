f722ea7a6990ccdc607cd7532b830692cb869fc8
James Yoder <james@jyoder.com>
2019-04-26 15:20:36 -0500

    fix up install script

--- run/INSTALL.sh
+++ run/INSTALL.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install python3 socat
 mkfifo ppengine_in 
 mkfifo pprx_out 
+mkdir output_files
 sudo cp ../src/install/ppengine.service /lib/systemd/system/ppengine.service
 sudo systemctl enable ppengine.service
 sudo systemctl start ppengine.service
